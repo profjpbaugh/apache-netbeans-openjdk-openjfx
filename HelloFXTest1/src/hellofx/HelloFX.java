912b6998663749be5c4b4b3f708b5bb185e816f2
John P. Baugh <jpbaugh@umich.edu>
2019-10-06 01:49:17 -0400

    Related projects
    Projects referred to in the documentation for installing OpenJDK, Apache NetBeans, OpenJFX (Open JavaFX), and Scene Builder

---
+++ HelloFXTest1/src/hellofx/HelloFX.java
@@ -0,0 +1,21 @@
+package hellofx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloFX extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
---
+++ HelloFXTest1/src/hellofx/HelloFXController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellofx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author profj
+ */
+public class HelloFXController implements Initializable {
+
+    @FXML
+    private Label theLabel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void doSomething(ActionEvent event) {
+        theLabel.setText("Hello World!  It worked!");
+    }
+    
+}
